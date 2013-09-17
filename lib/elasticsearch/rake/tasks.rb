2a91f1dc2c10c37aa49555f5915f4cd6edea5046
Felix Gilcher <felix.gilcher@asquera.de>
2013-09-17 15:30:50 +0000

    bare gem structure as created by bundler

---
+++ lib/elasticsearch/rake/tasks.rb
@@ -0,0 +1,9 @@
+require "elasticsearch/rake/tasks/version"
+
+module Elasticsearch
+  module Rake
+    module Tasks
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/elasticsearch/rake/tasks/version.rb
@@ -0,0 +1,7 @@
+module Elasticsearch
+  module Rake
+    module Tasks
+      VERSION = "0.0.1"
+    end
+  end
+end
