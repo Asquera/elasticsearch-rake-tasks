dc5cf50e859f7404ac8d9ca6ce0df670902fda7a
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2014-03-13 15:33:19 +0100

    Wrapped reindex task in a class to handle arguments more clearly

--- lib/elasticsearch/rake/tasks/seeder.rb
+++ lib/elasticsearch/rake/tasks/seeder.rb
@@ -6,8 +6,9 @@ module Elasticsearch
         attr_reader :sender
 
         def initialize(args = {})
-          @server = args.fetch(:server){ raise "No es server given" }
-          @index  = args.fetch(:index){ raise "No es index given" }
+          opts = args.reject{ |k,v| v.nil? }
+          @server = opts.fetch(:server){ raise "No es server given" }
+          @index  = opts.fetch(:index){ raise "No es index given" }
           @sender = Elasticsearch::IO::ChunkedSender.new(sink)
         end
 
